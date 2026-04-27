ac0c84de93a0209669dc06418982facbd6110f17
Muhamad Fauzaan <muhamadfauzan4750@gmail.com>
2026-04-27 15:15:55 +0700

    feat: add Google Analytics tracking to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,9 +84,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const gaId = "G-FX8XQVQVDM";
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `}
+        </Script>
         <Script
           id="organization-schema"
           type="application/ld+json"
